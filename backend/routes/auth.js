b646917fdce07f7bb6ff79f58f4adf26441509f1
Sanskar <sanskartakkamore@gmail.com>
2022-06-15 03:12:32 +0530

    done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,22 +4,36 @@ const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 
 // Create User using: POST "/api/auth"
-router.post('/',[
+router.post('/createuser',[
     body('name','Enter a Valid Name').isLength({ min: 3 }),
     body('email','Enter a Valid Email').isEmail(),
     body('password','Passwords must be atleast 5 characters').isLength({ min: 5 }),
     
-],(req,res)=>{
+],async(req,res)=>{
+    //If there are errors, return Bad request and the errors 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email:req.body.email,
-      }).then(user => res.json(user))
-      .catch(err =>{console.log(err)
-    res.json({error:"Please Enter a Unique Email address",message: err.message})})
+
+    let user =await User.findOne({email:req.body.email});
+//check whweater the user with same email exists or not
+try {
+  
+  if (user){
+    return res.status(400).json({error: "Sorry the user with same email is already Exists"})
+  }
+  user =await User.create({
+    name: req.body.name,
+    password: req.body.password,
+    email:req.body.email,
+  })
+  
+  res.json(user)
+}
+catch (error) {
+  console.error(error.message);
+  res.status(500).send("Some Error Occured");
+} 
 })
 module.exports=router
