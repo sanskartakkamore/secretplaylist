64223696470241ac1b622ffe1d4003fc5da9fbc4
Sanskar <sanskartakkamore@gmail.com>
2022-06-17 12:16:45 +0530

    Login Api Added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,9 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 const JWT_SECRET = 'Sanskarisagoodb$oy';
-// Create User using: POST "/api/auth"
+
+
+// Route 1 : Create User using: POST "/api/auth/createuser"
 router.post('/createuser', [
   body('name', 'Enter a Valid Name').isLength({
     min: 3
@@ -48,11 +50,55 @@ router.post('/createuser', [
         id:user.id
        }
     }
-    const jwtData = jwt.sign(data,JWT_SECRET);
-    res.json(user)
+    const authtoken = jwt.sign(data,JWT_SECRET);
+
+    //res.json(user)
+    res.json({authtoken})
+
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Some Error Occured");
   }
+
 })
+//Route 2  //Authenticate User using Post: /api/auth/login
+  router.post('/login', [
+       body('email', 'Enter a Valid Email').isEmail(),
+       body('password', 'Password cannot be blank').exists(),
+  ],async (req, res) => {
+      //If there are errors, return Bad request and the errors 
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    });
+  }
+
+  //destructing email & Password from the create user
+  const {email, password} = req.body;
+  try {
+    let user = await User.findOne({email});
+    if (!user) {
+      return res.status(400).json({error:"Please try to login with correct credentials"})
+    }
+
+    const passwordCompare = await bcrypt.compare(password,user.password);
+    if (!passwordCompare) {
+      return res.status(400).json({error:"Please try to login with correct credentials"})
+    }
+  const data ={
+       user :{
+        id: user.id
+       }
+    }
+    const authtoken = jwt.sign(data,JWT_SECRET);
+    //console.log(authtoken )
+    res.json({authtoken})
+  } 
+  catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server error occured");
+  }
+  }),
+  
 module.exports = router
